d404be6bd08b1ecdee4570a481979fcc83059476
57312 <javier.ladino.mt@correo.usa.edu.co>
2021-11-08 17:14:07 -0500

    Correcion de orden

--- src/main/java/co/usa/ciclo3/rentcloud/ciclo3/model/Cloud.java
+++ src/main/java/co/usa/ciclo3/rentcloud/ciclo3/model/Cloud.java
@@ -22,9 +22,9 @@ public class Cloud implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
     private String brand;
     private Integer year;
-    private String name;
     private String description;
 
     @ManyToOne
